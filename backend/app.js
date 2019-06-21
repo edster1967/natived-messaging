b9a69fffb5e4b01c9623384cfb27fa836908af77
unknown <edwardnatividad@gmail.com>
2019-06-21 14:40:34 -0400

    added updated changes to support one app deploy

--- backend/app.js
+++ backend/app.js
@@ -8,17 +8,18 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-mongoose.connect('<ENTER URL STRING HERE>', { useNewUrlParser: true })
+mongoose.connect('<ENTER URL HERE>', { useNewUrlParser: true })
     .then(() => {
         console.log('connected to the database')
     }).catch(() => {
         console.log('connection failed')
     });
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
-
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/", express.static(path.join(__dirname, "angular")));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -34,5 +35,8 @@ app.use((req, res, next) => {
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
